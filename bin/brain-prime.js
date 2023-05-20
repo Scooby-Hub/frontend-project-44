a31481724dd5b1ea4cb5affcbafd4d4306e27dd2
Scooby <fbhn389hrs@gmail.com>
2023-05-20 18:51:49 +0300

    Change: fix bin/brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -2,35 +2,28 @@
 
 import { greetingsFuncExp, questionFuncExp, endsGameFuncExp } from '../src/index.js';
 
+// Иселючен ноль в функции 'randNumberFunc'
 const randNumberFunc = (measureNum) => (Math.trunc(Math.random() * (measureNum) + 1));
 
 const gameBrain = (randNumber) => {
-  // Иселючен ноль в функции 'randNumberFunc'
-
-  const randomNumber = randNumber; // Число для проверки простое/составное
   const sqRootArr = []; // Массив для чисел для проверки простое/составное
-  let rightAnswer = '';
 
   // Наполнение массива для чисел для проверки простое/составное, но без единицы
-  for (let i = 0; i < randomNumber - 1; i += 1) {
-    const sqrtNumber = Math.sqrt(randomNumber - i);
+  for (let i = 0; i < randNumber - 1; i += 1) {
+    const sqrtNumber = Math.sqrt(randNumber - i);
     if (sqrtNumber === Math.trunc(sqrtNumber)) {
       sqRootArr.unshift(sqrtNumber);
     }
   }
 
   // Счетчик 'counter' считает кол-во делителей
-  let counter = 0;
   for (let i = 0; i < sqRootArr.length; i += 1) {
-    if (randomNumber % sqRootArr[i] === 0) {
-      counter += 1;
+    if (randNumber % sqRootArr[i] === 0) {
+      return [randNumber, 'no'];
     }
   }
 
-  // Если counter = 0 - число простое, если counter > 0 - составное
-  rightAnswer = counter < 1 ? 'yes' : 'no';
-
-  return [randomNumber, rightAnswer];
+  return [randNumber, 'yes'];
 };
 
 const theGame = () => {
