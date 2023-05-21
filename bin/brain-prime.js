df3c2d5b918b07cffd9d7c7beb1c6741733b3160
Scooby <fbhn389hrs@gmail.com>
2023-05-21 12:01:03 +0300

    Change: fix bin/brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -5,7 +5,7 @@ import { greetingsFuncExp, questionFuncExp, endsGameFuncExp } from '../src/index
 // Иселючен ноль в функции 'randNumberFunc'
 const randNumberFunc = (measureNum) => (Math.trunc(Math.random() * (measureNum) + 1));
 
-const gameBrain = (randNumber) => {
+const fillSqRootArrA = (randNumber) => {
   const sqRootArr = []; // Массив для чисел для проверки простое/составное
 
   // Наполнение массива для чисел для проверки простое/составное, но без единицы
@@ -15,29 +15,29 @@ const gameBrain = (randNumber) => {
       sqRootArr.unshift(sqrtNumber);
     }
   }
+  return [randNumber, ...sqRootArr];
+};
 
+const gameBrain = (randNumberAndsqRootArr) => {
   // Счетчик 'counter' считает кол-во делителей
-  for (let i = 0; i < sqRootArr.length; i += 1) {
-    if (randNumber % sqRootArr[i] === 0) {
-      return [randNumber, 'no'];
+  for (let i = 1; i < randNumberAndsqRootArr.length; i += 1) {
+    if (randNumberAndsqRootArr[0] % randNumberAndsqRootArr[i] === 0) {
+      return [randNumberAndsqRootArr[0], 'no'];
     }
   }
 
-  return [randNumber, 'yes'];
+  return [randNumberAndsqRootArr[0], 'yes'];
 };
 
-const theGame = () => {
-  greetingsFuncExp('Answer "yes" if given number is prime. Otherwise answer "no".');
-  let counter = 0;
-  for (let i = 1; i < 4; i += 1) {
-    counter += questionFuncExp(gameBrain(randNumberFunc(100)));
-    if (counter < 0) {
-      i = 3;
-    }
-    if (counter === 3) {
-      endsGameFuncExp();
-    }
+greetingsFuncExp('Answer "yes" if given number is prime. Otherwise answer "no".');
+let counter = 0;
+for (let i = 1; i < 4; i += 1) {
+  const sqRootArr = fillSqRootArrA(randNumberFunc(100));
+  counter += questionFuncExp(gameBrain(sqRootArr));
+  if (counter < 0) {
+    i = 3;
   }
-};
-
-theGame();
+  if (counter === 3) {
+    endsGameFuncExp();
+  }
+}
