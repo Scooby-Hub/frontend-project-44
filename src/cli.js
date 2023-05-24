254a64bf093b1695898daeda53ecbfe93b85bf28
Scooby <fbhn389hrs@gmail.com>
2023-05-24 20:25:27 +0300

    Change: fix src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,7 @@ import readlineSync from 'readline-sync';
 
 const askingName = () => {
   // Wait for user's response.
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
