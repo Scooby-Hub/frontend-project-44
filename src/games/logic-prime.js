adec4b8401d60b10e453197450a79d9e369a0436
Scooby <fbhn389hrs@gmail.com>
2023-06-11 01:38:02 +0300

    Change: fix src/games/logic-prime.js

--- src/games/logic-prime.js
+++ src/games/logic-prime.js
@@ -12,7 +12,7 @@ const gameBrain = () => {
 
   let rightAnswerInStrType;
 
-  for (let i = 1; i <= sqRootArr.length; i += 1) {
+  for (let i = 0; i < sqRootArr.length; i += 1) {
     if (randNumber % sqRootArr[i] === 0) {
       rightAnswerInStrType = 'no';
       return [question, rightAnswerInStrType];
